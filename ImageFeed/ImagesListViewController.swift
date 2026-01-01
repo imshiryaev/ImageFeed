dc341667b701e132992e4cdef7e5e3a1bb0b7b8f
Denis <d.shiryaev111@yandex.ru>
2026-01-01 21:28:15 +0800

    Initial Commit

---
+++ ImageFeed/ImagesListViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImageFeed
+//
+//  Created by Denis Shiryaev on 01.01.2026.
+//
+
+import UIKit
+
+class ImagesListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
