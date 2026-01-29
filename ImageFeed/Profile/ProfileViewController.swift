15ea07540a9787b98cadf0b57481396ca349fd13
Denis Shiryaev <d.shiryaev111@yandex.ru>
2026-01-29 18:14:53 +0300

    add profile screen UI

--- ImageFeed/Profile/ProfileViewController.swift
+++ ImageFeed/Profile/ProfileViewController.swift
@@ -5,5 +5,11 @@ class ProfileViewController: UIViewController {
         super.viewDidLoad()
     }
 
-    
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var profileFullName: UILabel!
+    @IBOutlet weak var profileUsername: UILabel!
+    @IBOutlet weak var profileDescription: UILabel!
+    @IBOutlet weak var profileLogoutButton: UIButton!
+    @IBAction func tapOnLogoutButton(_ sender: Any) {
+    }
 }
